eae7cf0ebe627c440dea5b6f6cb6a6d6cbd71620
Aravind <aravindkumar.indus@gmail.com>
2025-10-25 00:34:40 +0530

    commit

--- Final/twilio_alerts.py
+++ Final/twilio_alerts.py
@@ -2,10 +2,10 @@
 from twilio.rest import Client
 
 # Twilio credentials (replace with your own)
-ACCOUNT_SID = "AC855c77cab687272ab71ca1d72d0a36d3"
-AUTH_TOKEN = "18a40942933cfe1b63fce2c49eadcf97"
-FROM_NUMBER = "+19789589288"   # Twilio phone number
-TO_NUMBER = "+91 891914648"   # Your phone number
+ACCOUNT_SID = "TwilioAccount_SID"
+AUTH_TOKEN = "TwilioAuthToken"
+FROM_NUMBER = "+123456789"   # Twilio phone number
+TO_NUMBER = "+91xxxxxxxxx"   # Your phone number
 
 client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
@@ -15,3 +15,4 @@ def send_sms_alert(message):
         from_=FROM_NUMBER,
         to=TO_NUMBER
     )
+
