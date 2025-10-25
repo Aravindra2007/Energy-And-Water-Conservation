ff8158a58cc93236da98d47fd7a6b36607363bae
Aravind <aravindkumar.indus@gmail.com>
2025-10-25 10:51:35 +0530

    commit

--- Final/src/recommendations.py
+++ Final/src/recommendations.py
@@ -2,8 +2,8 @@
 from twilio_alerts import send_sms_alert
 
 # Cut-off limits
-ENERGY_LIMIT = 3000  # Wh
-WATER_LIMIT = 100     # liters
+ENERGY_LIMIT = 5000  # Wh
+WATER_LIMIT = 50     # liters
 
 # Track which appliances have been alerted (avoid duplicate SMS)
 sent_energy_alerts = set()
@@ -42,5 +42,6 @@ def get_water_recommendations(df, max_tips=10):
         if len(tips) >= max_tips:
             break
     return tips
+
 
 
